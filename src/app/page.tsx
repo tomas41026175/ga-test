56a639d13bb7478c57d42c30b0f71f82e0cc35f7
tomas_chang <tomas_chang@mayohr.com>
2024-05-09 15:32:55 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
+import { initializeGoogleTagManager } from "./test/googleTagManager";
+
 // import Image from "next/image";
 
 export default function Home() {
+  useEffect(() => {
+    // Initialize Google Tag Manager with your GTM ID
+    initializeGoogleTagManager("GTM-WT8FS7X");
+
+    console.log("ga initialized");
+  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>home</div>
